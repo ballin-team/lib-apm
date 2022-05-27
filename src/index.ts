af84177c4bc2c9db34a12e6ab1c2cfa34f35dad6
Renan <renan.soares.francisco@gmail.com>
2022-05-26 21:45:30 -0300

    feat(sentry): adding sentry transport

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+// Importing @sentry/tracing patches the global hub for tracing to work.
+export * as SentryTracing from "@sentry/tracing";
+export * as Sentry from "@sentry/node";
 export * from './logger';
